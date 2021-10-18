0f405a4a118782dccebb94fa1e5a8bb1243ca741
HakuzosuSAMA <3172906506@qq.com>
2021-10-18 20:07:09 +0800

    ADD BinsarySearchTree_Wiki + RedBlackTree

---
+++ BinarySearchTree_Wiki.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+template <typename T> class BinarySearchTree {
+
+public:
+  class Node {
+  public:
+    T value;
+    Node *left;
+    Node *right;
+
+  public:
+    Node(T value, Node *left, Node *right)
+        : value(value), left(left), right(right) {}
+
+  public:
+    Node(T value) : Node(value, nullptr, nullptr) {}
+  };
+
+public:
+  Node *root = nullptr;
+
+public:
+  Node *search_recursively(Node *root, T value) {
+    if (value == root->value)
+      return root;
+    return search_iteratively(value < root->value ? root->left : root->right,
+                              value);
+  }
+
+public:
+  Node *search_iteratively(Node *root, T value) {
+    while (root != nullptr && root->value != value) {
+      root = value < root->value ? root->left : root->right;
+    }
+    return root;
+  }
+
+public:
+  void preorder_walk(Node *root) {
+    if (root == nullptr)
+      return;
+    cout << root->value << " ";
+    if (root->left != nullptr)
+      preorder_walk(root->left);
+    if (root->right != nullptr)
+      preorder_walk(root->right);
+  }
+
+public:
+  void inorder_walk(Node *root) {
+    if (root == nullptr)
+      return;
+    if (root->left != nullptr)
+      inorder_walk(root->left);
+    cout << root->value << " ";
+    if (root->right != nullptr)
+      inorder_walk(root->right);
+  }
+
+public:
+  void postorder_walk(Node *root) {
+    if (root == nullptr)
+      return;
+    if (root->left != nullptr)
+      postorder_walk(root->left);
+    if (root->right != nullptr)
+      postorder_walk(root->right);
+    cout << root->value << " ";
+  }
+
+public:
+  void insert_node(Node *&root, T value) {
+    if (root == nullptr) {
+      root = new Node(value);
+      return;
+    }
+
+    insert_node(value < root->value ? root->left : root->right, value);
+  }
+
+public:
+  Node *min_node(Node *root) {
+    return root->left ? min_node(root->left) : root;
+  }
+
+public:
+  Node *max_node(Node *root) {
+    return root->right ? max_node(root->right) : root;
+  }
+
+public:
+  Node *replace_node_in_parent(Node *root, Node *child) {}
+
+public:
+  void delete_node(Node *&root, T value) {
+
+    // check nullptr.
+    if (root == nullptr)
+      return;
+
+    // locate delete target.
+    if (value == root->value) {
+
+      if (root->left == nullptr && root->right == nullptr)
+        root = nullptr;
+      else if (root->left != nullptr) {
+        root->value = max_node(root->left)->value;
+        delete_node(root->left, root->value);
+      } else {
+        root->value = min_node(root->right)->value;
+        delete_node(root->right, root->value);
+      }
+
+    } else
+      delete_node(value < root->value ? root->left : root->right, value);
+  }
+};
+
+int main() {
+
+  BinarySearchTree<int> bst;
+  bst.insert_node(bst.root, 100);
+  bst.insert_node(bst.root, 200);
+  bst.insert_node(bst.root, 400);
+  bst.insert_node(bst.root, 300);
+
+  bst.inorder_walk(bst.root);
+  printf("\n");
+  bst.delete_node(bst.root, 100);
+  bst.inorder_walk(bst.root);
+  printf("\n");
+
+  return 0;
+}
--- RedBlackTree.cpp
+++ RedBlackTree.cpp
@@ -16,67 +16,392 @@ enum Color { RED, BLACK };
 
 template <typename T> class RedBlackTree {
 
-  class Node {
-  public:
-    T value;
-    Color color;
-    Node *parent;
-    Node *left;
-    Node *right;
+    class Node {
+    public:
+        T value;
+        Color color;
+        Node *parent;
+        Node *left;
+        Node *right;
 
-  public:
-    Node(T value, Color color, Node *parent, Node *left, Node *right)
-        : value(value), color(color), parent(parent), left(left), right(right) {
+    public:
+        Node(T value, Color color, Node *parent, Node *left, Node *right)
+                : value(value), color(color), parent(parent), left(left), right(right) {
+        }
+
+    public:
+        Node(T value, Color color)
+                : Node(value, color, nullptr, nullptr, nullptr) {}
+
+    public:
+        Node *grand_parent() { return this->parent->parent; }
+
+    public:
+        Node *uncle() { return this->parent->sibling(); }
+
+    public:
+        Node *sibling() {
+            return this == this->parent->left ? this->parent->right
+                                              : this->parent->left;
+        }
+    };
+
+public:
+    Node *root = nullptr;
+    // rank: 从 该root节点 到 叶子节点 路径中 黑色指针的数量
+    // 结论: 从 path(root, leaf).length <= 2 * rank(root);
+    // 结论: h(root) <= 2 * rank(root)
+    // 结论: r <= log2(n + 1)
+public:
+    int rank(Node *root) { return -1; }
+
+public:
+    void inorder_walk(Node *root) {
+        if (root == nullptr)
+            return;
+        if (root->left != nullptr)
+            inorder_walk(root->left);
+        cout << root->value << " ";
+        if (root->right != nullptr)
+            inorder_walk(root->right);
     }
 
-  public:
-    Node(T value, Color color)
-        : Node(value, color, nullptr, nullptr, nullptr) {}
-
-  public:
-    Node *grand_parent() { return this->parent->parent; }
-
-  public:
-    Node *uncle() { return this->parent->sibling(); }
-
-  public:
-    Node *sibling() {
-      return this == this->parent->left ? this->parent->right
-                                        : this->parent->left;
+public:
+    Node *search_node(Node *root, T value) {
+        if (root == nullptr)
+            return nullptr;
+        if (value == root->value)
+            return root;
+        return search_node(value < root->value ? root->left : root->right, value);
     }
-  };
 
 public:
-  Node *root = nullptr;
-  // rank: 从 该root节点 到 叶子节点 路径中 黑色指针的数量
-  // 结论: 从 path(root, leaf).length <= 2 * rank(root);
-  // 结论: h(root) <= 2 * rank(root)
-  // 结论: r <= log2(n + 1)
-public:
-  int rank(Node *root) { return -1; }
+    void rotate_right(Node *p){
+        Node *gp = p->grand_parent();
+        Node *fa = p->parent;
+        Node *y = p->right;
+
+        fa->left= y;
+
+        if(y != nullptr)
+            y->parent = fa;
+        p->right= fa;
+        fa->parent = p;
+
+        if(root == fa)
+            root = p;
+        p->parent = gp;
+
+        if(gp != NULL){
+            if(gp->left== fa)
+                gp->left= p;
+            else
+                gp->right= p;
+        }
+
+    }
 
 public:
-  void inorder_walk(Node *root) {
-    if (root == nullptr)
-      return;
-    if (root->left != nullptr)
-      inorder_walk(root->left);
-    cout << root->value << " ";
-    if (root->right != nullptr)
-      inorder_walk(root->right);
-  }
+    void rotate_left(Node *p){
+        if(p->parent == NULL){
+            root = p;
+            return;
+        }
+        Node *gp = p->grand_parent();
+        Node *fa = p->parent;
+        Node *y = p->left;
+
+        fa->right= y;
+
+        if(y != nullptr)
+            y->parent = fa;
+        p->left= fa;
+        fa->parent = p;
+
+        if(root == fa)
+            root = p;
+        p->parent = gp;
+
+        if(gp != NULL){
+            if(gp->left== fa)
+                gp->left= p;
+            else
+                gp->right= p;
+        }
+    }
+public:
+    Color get_color(Node *root) {
+        if (root == nullptr)
+            return Color::BLACK;
+        return root->color;
+    }
 
 public:
-  Node *search_node(Node *root, T value) {
-    if (root == nullptr)
-      return nullptr;
-    if (value == root->value)
-      return root;
-    return search_node(value < root->value ? root->left : root->right, value);
-  }
+    void insert_node(Node *&root, T value) {
+        // construct target node.
+        Node *target = new Node(value, Color::RED);
+
+        // find insert position.
+        Node *target_parent = nullptr;
+        Node *aux_node = root;
+        while (aux_node != nullptr) {
+            target_parent = aux_node;
+            target->value < aux_node->value ? aux_node = aux_node->left
+                                            : aux_node = aux_node->right;
+        }
+
+        // insert target node into y'left or right
+        target->parent = target_parent;
+        if (target_parent == nullptr)
+            root = target;
+        else
+            target->value < target_parent->value ? target_parent->left = target
+                                                 : target_parent->right = target;
+
+        // rebalance
+        insert_case1(target);
+    }
 
 public:
-  void rotate_left(Node *root) {
+    void insert_case1(Node *n) {
+        printf("insert_case1\n");
+        if (n->parent == nullptr)
+            n->color = Color::BLACK;
+        else
+            insert_case2(n);
+    }
+
+public:
+    void insert_case2(Node *n) {
+        printf("insert_case2\n");
+        if (n->parent->color == Color::BLACK)
+            return;
+        else
+            insert_case3(n);
+    }
+
+public:
+    void insert_case3(Node *n) {
+        printf("insert_case3\n");
+        if (n->uncle() != nullptr && n->uncle()->color == Color::RED) {
+
+            n->parent->color = Color::BLACK;
+            n->uncle()->color = Color::BLACK;
+            n->grand_parent()->color = Color::RED;
+
+            insert_case1(n->grand_parent());
+        } else
+            insert_case4(n);
+    }
+
+public:
+    void insert_case4(Node *n) {
+        printf("insert_case4\n");
+        if (n == n->parent->right && n->parent == n->grand_parent()->left) {
+            rotate_left(n);
+            n = n->left;
+        } else if (n == n->parent->left && n->parent == n->grand_parent()->right) {
+            rotate_right(n);
+            n = n->right;
+        }
+
+        insert_case5(n);
+    }
+
+public:
+    void insert_case5(Node *n) {
+        printf("insert_case5\n");
+        n->parent->color = Color::BLACK;
+        n->grand_parent()->color = Color::RED;
+        if (n == n->parent->left && n->parent == n->grand_parent()->left) {
+            rotate_right(n->parent);
+        } else {
+            rotate_left(n->parent);
+        }
+    }
+
+public:
+    void delete_case1(Node *n) {
+        printf("delete_case1\n");
+        if (n->parent != nullptr)
+            delete_case2(n);
+    }
+
+public:
+    void delete_case2(Node *n) {
+        printf("delete_case2\n");
+        Node *s = n->sibling();
+
+        if (s->color == Color::RED) {
+            n->parent->color = Color::RED;
+            s->color = Color::BLACK;
+
+            if (n == n->parent->left) {
+                rotate_left(n->parent);
+            } else
+                rotate_right(n->parent);
+        }
+
+        delete_case3(n);
+    }
+
+public:
+    void delete_case3(Node *n) {
+        printf("delete_case3\n");
+        Node *s = n->sibling();
+
+        if ((n->parent->color == Color::BLACK) && (s->color == Color::BLACK) &&
+            (s->left->color == Color::BLACK) && (s->right->color == Color::BLACK)) {
+            s->color = Color::RED;
+            delete_case1(n->parent);
+        } else
+            delete_case4(n);
+    }
+
+public:
+    void delete_case4(Node *n) {
+        printf("delete_case4\n");
+        Node *s = n->sibling();
+        if ((n->parent->color == Color::RED) && (s->color == Color::BLACK) &&
+            (s->left->color == Color::BLACK) && (s->right->color == Color::BLACK)) {
+            s->color = Color::RED;
+            n->parent->color = Color::BLACK;
+        } else
+            delete_case5(n);
+    }
+
+public:
+    void delete_case5(Node *n) {
+        printf("delete_case5\n");
+        Node *s = n->sibling();
+
+        if (s->color == Color::BLACK) {
+            if ((n == n->parent->left) && (s->right->color == Color::BLACK) &&
+                (s->left->color == Color::RED)) {
+                s->color = Color::RED;
+                s->left->color = Color::BLACK;
+                rotate_right(s);
+            } else if ((n == n->parent->right) && (s->left->color == BLACK) &&
+                       (s->right->color == RED)) {
+                s->color = RED;
+                s->right->color = BLACK;
+                rotate_left(s);
+            }
+        }
+        delete_case6(n);
+    }
+
+public:
+    void delete_case6(Node *n) {
+        printf("delete_case6\n");
+        Node *s = n->sibling();
+        s->color = n->parent->color;
+        n->parent->color = Color::BLACK;
+
+        if (n == n->parent->left) {
+            s->right->color = Color::BLACK;
+            rotate_left(n->parent);
+        } else {
+            s->left->color = Color::BLACK;
+            rotate_right(n->parent);
+        }
+    }
+
+public:
+    Node *min_node(Node *root) {
+        while (root->left != nullptr)
+            root = root->left;
+        return root;
+    }
+
+public:
+    Node *delete_min(Node *root) {
+        if (root->left == nullptr)
+            return root->right;
+        root->left = delete_min(root->left);
+        return root;
+    }
+
+public:
+    bool delete_value(T value){
+        return delete_node(this->root, value);
+    }
+
+public:
+    void delete_tree(Node * root) {
+        if (root == nullptr) return;
+        delete_tree(root->left);
+        delete_tree(root->right);
+        delete root;
+    }
+
+
+
+public:
+    void replace_node(Node * node, Node * child) {
+        // case: the tree is only 1 node.
+        if (node == this->root && node->left == nullptr && node->right == nullptr) {
+            this->root = nullptr;
+            return;
+        }
+
+        // link parents
+        if (node->parent->left == node) {
+            node->parent->left = child;
+        } else {
+            node->parent->right = child;
+        }
+        child->parent = node->parent;
+    }
+
+
+public:
+    void delete_only_child(Node * node) {
+
+        // find the only child of node.
+        Node * node_child = node->left == nullptr ? node->right : node->left;
+
+        if(node->parent == nullptr && node->left== nullptr && node->right== nullptr){
+            node = nullptr;
+            this->root = node;
+            return;
+        }
+
+        // replace node and the only child of node.
+//        replace_node(node, node_child);
+        if(node->parent == nullptr){
+            delete  node;
+            node_child->parent = nullptr;
+            this->root = node_child;
+            this->root->color = Color::BLACK;
+            return;
+        }
+
+        if(node->parent->left== node){
+           node->parent->left= node_child;
+        } else {
+            node->parent->right= node_child;
+        }
+
+        // is node_child nullptr ?
+        // TODO delete 
+        if (node_child != nullptr) {
+            node_child->parent = node->parent;
+
+            // color
+            if (node->color == Color::BLACK) {
+                if (node_child->color == Color::RED) node_child->color = Color::BLACK;
+                else delete_case1(node_child);
+            }
+
+            // free node
+            delete node;
+        }
+
+    }
+
+public:
+  void rotate_left_V2(Node *root) {
     Node *root_right = root->right;
     Node *root_right_left = root->right->left;
     Node *root_parent = root->parent;
@@ -94,7 +419,7 @@ public:
   }
 
 public:
-  void rotate_right(Node *root) {
+  void rotate_right_V2(Node *root) {
     Node *root_left = root->left;
     Node *root_left_right = root->left->right;
     Node *root_parent = root->parent;
@@ -112,244 +437,50 @@ public:
   }
 
 public:
-  Color get_color(Node *root) {
-    if (root == nullptr)
-      return Color::BLACK;
-    return root->color;
-  }
+    void delete_node(Node * root, T value) {
+        // can't locate delete position.
+        if (root == nullptr) return ;
 
-public:
-  void insert_node(Node *&root, T value) {
-    // construct target node.
-    Node *target = new Node(value, Color::RED);
 
-    // find insert position.
-    Node *target_parent = nullptr;
-    Node *aux_node = root;
-    while (aux_node != nullptr) {
-      target_parent = aux_node;
-      target->value < aux_node->value ? aux_node = aux_node->left
-                                      : aux_node = aux_node->right;
+        // locate delete position.
+        if (value < root->value) {
+            delete_node(root->left, value);
+        } else if (value > root->value) {
+            delete_node(root->right, value);
+        } else if (value == root->value) {
+
+            // why check right ?
+            if (root->right == nullptr) {
+                delete_only_child(root);
+                return;
+            }
+
+            Node * smallest = min_node(root->right);
+            swap(root->value, smallest->value);
+
+            delete_only_child(smallest);
+        }
+
     }
 
-    // insert target node into y'left or right
-    target->parent = target_parent;
-    if (target_parent == nullptr)
-      root = target;
-    else
-      target->value < target_parent->value ? target_parent->left = target
-                                           : target_parent->right = target;
 
-    // rebalance
-    insert_case1(target);
-  }
-
-public:
-  void insert_case1(Node *n) {
-    printf("insert_case1\n");
-    if (n->parent == nullptr)
-      n->color = Color::BLACK;
-    else
-      insert_case2(n);
-  }
-
-public:
-  void insert_case2(Node *n) {
-    printf("insert_case2\n");
-    if (n->parent->color == Color::BLACK)
-      return;
-    else
-      insert_case3(n);
-  }
-
-public:
-  void insert_case3(Node *n) {
-    printf("insert_case3\n");
-    if (n->uncle() != nullptr && n->uncle()->color == Color::RED) {
-
-      n->parent->color = Color::BLACK;
-      n->uncle()->color = Color::BLACK;
-      n->grand_parent()->color = Color::RED;
-
-      insert_case1(n->grand_parent());
-    } else
-      insert_case4(n);
-  }
-
-public:
-  void insert_case4(Node *n) {
-    printf("insert_case4\n");
-    if (n == n->parent->right && n->parent == n->grand_parent()->left) {
-      rotate_left(n);
-      n = n->left;
-    } else if (n == n->parent->left && n->parent == n->grand_parent()->right) {
-      rotate_right(n);
-      n = n->right;
-    }
-
-    insert_case5(n);
-  }
-
-public:
-  void insert_case5(Node *n) {
-    printf("insert_case5\n");
-    n->parent->color = Color::BLACK;
-    n->grand_parent()->color = Color::RED;
-    if (n == n->parent->left && n->parent == n->grand_parent()->left) {
-      rotate_right(n->parent);
-    } else {
-      rotate_left(n->parent);
-    }
-  }
-
-public:
-  void delete_case1(Node *n) {
-    printf("delete_case1\n");
-    if (n->parent != nullptr)
-      delete_case2(n);
-  }
-
-public:
-  void delete_case2(Node *n) {
-    printf("delete_case2\n");
-    Node *s = n->sibling();
-
-    if (s->color == Color::RED) {
-      n->parent->color == Color::RED;
-      s->color = Color::BLACK;
-
-      if (n == n->parent->left) {
-        rotate_left(n->parent);
-      } else
-        rotate_right(n->parent);
-    }
-
-    delete_case3(n);
-  }
-
-public:
-  void delete_case3(Node *n) {
-    printf("delete_case3\n");
-    Node *s = n->sibling();
-
-    if ((n->parent->color == Color::BLACK) && (s->color == Color::BLACK) &&
-        (s->left->color == Color::BLACK) && (s->right->color == Color::BLACK)) {
-      s->color = Color::RED;
-      delete_case1(n->parent);
-    } else
-      delete_case4(n);
-  }
-
-public:
-  void delete_case4(Node *n) {
-    printf("delete_case4\n");
-    Node *s = n->sibling();
-    if ((n->parent->color == Color::RED) && (s->color == Color::BLACK) &&
-        (s->left->color == Color::BLACK) && (s->right->color == Color::BLACK)) {
-      s->color = Color::RED;
-      n->parent->color = Color::BLACK;
-    } else
-      delete_case5(n);
-  }
-
-public:
-  void delete_case5(Node *n) {
-    printf("delete_case5\n");
-    Node *s = n->sibling();
-
-    if (s->color == Color::BLACK) {
-      if ((n == n->parent->left) && (s->right->color == Color::BLACK) &&
-          (s->left->color == Color::RED)) {
-        s->color = Color::RED;
-        s->left->color = Color::BLACK;
-        rotate_right(s);
-      } else if ((n == n->parent->right) && (s->left->color == BLACK) &&
-                 (s->right->color == RED)) {
-        s->color = RED;
-        s->right->color = BLACK;
-        rotate_left(s);
-      }
-    }
-    delete_case6(n);
-  }
-
-public:
-  void delete_case6(Node *n) {
-    printf("delete_case6\n");
-    Node *s = n->sibling();
-    s->color = n->parent->color;
-    n->parent->color = Color::BLACK;
-
-    if (n == n->parent->left) {
-      s->right->color = Color::BLACK;
-      rotate_left(n->parent);
-    } else {
-      s->left->color = Color::BLACK;
-      rotate_right(n->parent);
-    }
-  }
-
-public:
-  Node *min_node(Node *root) {
-    while (root->left != nullptr)
-      root = root->left;
-    return root;
-  }
-
-public:
-  Node *delete_min(Node *root) {
-    if (root->left == nullptr)
-      return root->right;
-    root->left = delete_min(root->left);
-    return root;
-  }
-
-public:
-  Node *delete_node(Node *root, T value) {
-
-    if (root == nullptr)
-      return nullptr;
-
-    // locate target Node.
-    if (value < root->value)
-      root->left = delete_node(root->left, value);
-    else if (value > root->value)
-      root->right = delete_node(root->right, value);
-    else {
-      if (root->right == nullptr)
-        return root->left;
-      if (root->left == nullptr)
-        return root->right;
-
-      Node *t = root;
-      root = min_node(t->right);
-      root->right = delete_min(t->right);
-      root->left = t->left;
-    }
-
-    // rebalance
-    //delete_case1(root);
-    // TODO something about delete node.
-
-    // return root.
-    return root;
-  }
 };
+
 
 int main() {
 
-  // construct rbt and add elements.
-  RedBlackTree<int> rbt;
-  rbt.insert_node(rbt.root, 400);
-  rbt.insert_node(rbt.root, 300);
-  rbt.insert_node(rbt.root, 100);
-  rbt.insert_node(rbt.root, 200);
+    // construct rbt and add elements.
+    RedBlackTree<int> rbt;
+    rbt.insert_node(rbt.root, 400);
+    rbt.insert_node(rbt.root, 300);
+    rbt.insert_node(rbt.root, 100);
+    rbt.insert_node(rbt.root, 200);
 
-  rbt.inorder_walk(rbt.root);
-  printf("\n");
+    rbt.inorder_walk(rbt.root);
+    printf("\n");
 
-  // delete elements.
-  rbt.delete_node(rbt.root, 300);
-  rbt.inorder_walk(rbt.root);
-  return 0;
+    // delete elements.
+    rbt.delete_node(rbt.root, 200);
+    rbt.inorder_walk(rbt.root);
+    return 0;
 }
