428ecc140993ca318a4f17c7b631aba7abd621a7
HakuzosuSAMA <3172906506@qq.com>
2021-10-24 13:05:57 +0800

    MODIFY Queue

--- Queue.cpp
+++ Queue.cpp
@@ -4,62 +4,62 @@
 template <typename T> class Queue {
 
 private:
-  static const int DEFAULT_CAPACITY = 10000;
+    static const int DEFAULT_A_SIZE = 10000;
 
 private:
-  int capacity;
-  int head, tail;
-  T *Q;
+    int A_SIZE;
+    int head, tail; // with super tail element.
+    T *A;
 
 private:
-  void init() {
-    Q = (T *)malloc(capacity * sizeof(T));
-    head = tail = 0;
-  }
+    void init() {
+        A = (T *)malloc(A_SIZE * sizeof(T));
+        head = tail = 0;
+    }
 
 public:
-  Queue() {
-    capacity = DEFAULT_CAPACITY;
-    init();
-  }
+    Queue() {
+        A_SIZE = DEFAULT_A_SIZE;
+        init();
+    }
 
 public:
-  Queue(int n) {
-    capacity = n + 1;
-    init();
-  }
+    Queue(int queue_capacity) {
+        this->A_SIZE = queue_capacity + 1; // NOTE: an array[100] can only save 99 elements in queue.
+        init();
+    }
 
 public:
-  void enqueue(T x) {
-    Q[tail] = x;
-    tail = (tail + 1) % capacity;
-  }
+    void enqueue(T x) {
+        A[tail] = x;
+        tail = (tail + 1) % A_SIZE;
+    }
 
 public:
-  T dequeue() {
-    T x = Q[head];
-    head = (head + 1) % capacity;
-    return x;
-  }
+    T dequeue() {
+        T x = A[head];
+        head = (head + 1) % A_SIZE;
+        return x;
+    }
 
 public:
-  bool isFull() { return head == (tail + 1) % capacity; }
+    bool isFull() { return head == (tail + 1) % A_SIZE; }
 
 public:
-  bool isEmpty() { return head == tail; }
+    bool isEmpty() { return head == tail; }
 };
 
 int main() {
 
-  Queue<int> Q;
+    Queue<int> Q;
 
-  Q.enqueue(1);
-  Q.enqueue(2);
-  Q.enqueue(3);
+    Q.enqueue(1);
+    Q.enqueue(2);
+    Q.enqueue(3);
 
-  printf("dequeue element: %d\n", Q.dequeue());
-  printf("dequeue element: %d\n", Q.dequeue());
-  printf("dequeue element: %d\n", Q.dequeue());
+    printf("dequeue element: %d\n", Q.dequeue());
+    printf("dequeue element: %d\n", Q.dequeue());
+    printf("dequeue element: %d\n", Q.dequeue());
 
-  return 0;
+    return 0;
 }
